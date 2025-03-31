88aa26d7b50ec9f2540beade81c86d5d2f02ba92
anna-dahlberg <anna.dahlberg@live.se>
2025-03-31 22:43:26 +0200

    further developed models

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Company extends User
+class Company extends Model
 {
-    //
+    use HasFactory;
+
+    public function competences()
+    {
+        return $this->belongsToMany(Competence::class, 'company_competence_junction');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Student extends User
+class Student extends Model
 {
-    //
+    use HasFactory;
+
+    public function classModel()
+    {
+        return $this->belongsTo(YrgoClass::class);
+    }
+
+    public function competences()
+    {
+        return $this->belongsToMany(Competence::class, 'student_competence_junction');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }

f038e4f5787e449dd5acfd6f84826b6c32570849
anna-dahlberg <anna.dahlberg@live.se>
2025-04-01 15:04:48 +0200

    added seeders for students and companies

---
+++ database/seeders/CompaniesTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Company;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class CompanySeeder extends Seeder
+{
+    public function run()
+    {
+        $faker = Faker::create();
+
+        Company::create([
+            'image_url' => $faker->imageUrl(),
+            'website_url' => $faker->url,
+            'description' => $faker->paragraph,
+            'linkedin_url' => $faker->url,
+            'user_id' => 3,
+        ]);
+
+
+        Company::create([
+            'image_url' => $faker->imageUrl(),
+            'website_url' => $faker->url,
+            'description' => $faker->paragraph,
+            'linkedin_url' => $faker->url,
+            'user_id' => 4,
+        ]);
+    }
+}

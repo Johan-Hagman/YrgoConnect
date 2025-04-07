27db0ecba408f821bccab98c25e42c7ee05d53e4
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-04-07 12:36:08 +0200

    styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,18 +5,3 @@ import Alpine from "alpinejs";
 window.Alpine = Alpine;
 
 Alpine.start();
-
-document.addEventListener("DOMContentLoaded", () => {
-    const toggle = document.getElementById("dropdownToggle");
-    const menu = document.getElementById("dropdownMenu");
-
-    toggle?.addEventListener("click", () => {
-        menu.classList.toggle("hidden");
-    });
-
-    document.addEventListener("click", (e) => {
-        if (!toggle.contains(e.target) && !menu.contains(e.target)) {
-            menu.classList.add("hidden");
-        }
-    });
-});

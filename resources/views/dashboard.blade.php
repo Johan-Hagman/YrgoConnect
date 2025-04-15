c6e36cf433b6010506d78bf693664a9c33461673
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-04-15 13:44:49 +0200

    added cards to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,14 @@
 <x-public-layout>
     
     <x-hero-section />
+
     <x-event-info />
+
+
+    @if(auth()->user()->role->name === 'Company')
+    <x-student-cards-section />
+@elseif(auth()->user()->role->name === 'Student')
+    <x-company-cards-section />
+@endif
 
 </x-public-layout>
